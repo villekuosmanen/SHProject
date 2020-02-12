7fe3dfcc7ccbb7ca5a8a2eebff63cb64e652a5fe
Ville Kuosmanen <vibe96@gmail.com>
2020-02-12 18:21:09 +0000

    Add all types of explanations as a response

--- src/app.py
+++ src/app.py
@@ -10,9 +10,11 @@ import json
 import datetime
 import copy
 import pickle
+import requests
+import random
 
 from recommender_algo.editable_svd import EditableSVD
-from explanation import InfluenceExplainer
+from explanation import InfluenceExplainer, AssociationRulesExplainer
 
 app = Flask(__name__)
 
@@ -21,11 +23,27 @@ user_algo = None
 movies_map = {}
 user_rated_items = None
 association_rules = None
+moviedb_ids = {}
+api_key = ""
 
 @app.route('/movies')
 def get_movies():
     moviesList = [{'key': key, 'title': value} for key, value in movies_map.items()]
     return {'movies': moviesList}
+
+@app.route('/movies/details/<int:movie_id>')
+def get_movie_details(movie_id):
+    if movie_id not in moviedb_ids:
+        return jsonify(success=False)
+    link_id = moviedb_ids[movie_id]
+    movie_details = requests.get('https://api.themoviedb.org/3/movie/' + link_id, params={'api_key': api_key}).json()
+    poster_path = movie_details['poster_path']
+    description = movie_details['overview']
+
+    release_dates = requests.get('https://api.themoviedb.org/3/movie/' + link_id + 
+        '/release_dates', params={'api_key': api_key}).json()
+    age_rating = get_uk_age_rating(release_dates)
+    return {'poster_path': poster_path, 'description': description, 'age_rating': age_rating}
 
 @app.route('/movies/<int:user_id>/responses', methods=['POST'])
 def post_movie_responses(user_id):
@@ -33,7 +51,6 @@ def post_movie_responses(user_id):
     global user_algo
 
     responses = request.json
-    print(responses)
 
     # Retrain model, and save results
     user_rated_items = {rated_movie['key']: rated_movie['rating'] for rated_movie in responses['response']}
@@ -44,29 +61,70 @@ def post_movie_responses(user_id):
 
 @app.route('/recommendations/<int:user_id>/recommendations')
 def get_recommendations(user_id):
-    recommendations = top_n_recommendations(user_id)
+    recommendations = top_recommendations(user_id)
 
+    association_rules_explainer = AssociationRulesExplainer(user_rated_items, association_rules)
     recommendations_to_send = []
+    used_recommendations = []
     for x in recommendations:
-        movie_obj = {}
-        movie_obj['movieId'] = int(x[0])
-        movie_obj['title'] = movies_map[int(x[0])]
-        
-        # Generate explanation type A
-        # explainer = AssociationRulesExplainer(user_id, user_rated_items, user_algo)
-        # explanation = explainer.generate_explanation(x)
-        # movie_obj['explanation'] = [{'movieId': int(movie_id), 'title': movies_map[int(movie_id)]} for movie_id in explanation]
+        explanation = association_rules_explainer.generate_explanation(x)
+        if explanation is not None:
+            movie_obj = {}
+            movie_obj['movieId'] = x[0]
+            movie_obj['title'] = movies_map[x[0]]
+            movie_obj['explanation'] = {}
+            movie_obj['explanation']['rule'] = [{'movieId': int(movie_id), 'title': movies_map[int(movie_id)]} for movie_id in explanation]
+            movie_obj['explanation']['type'] = 'B'
+            recommendations_to_send.append(movie_obj)
+            used_recommendations.append(x[0])
+            if len(recommendations_to_send) >= 5:
+                break
 
-        # Generate explanation type B
-        explainer = InfluenceExplainer(user_id, user_rated_items, user_algo)
-        positives, negatives = explainer.generate_explanation(x)
-        movie_obj['explanation'] = {}
-        movie_obj['explanation']['positives'] = [
-            {'movieId': int(movie_id), 'title': movies_map[int(movie_id)], 'influence': influence} for movie_id, influence in positives]
-        movie_obj['explanation']['negatives'] = [
-            {'movieId': int(movie_id), 'title': movies_map[int(movie_id)], 'influence': influence} for movie_id, influence in negatives]
+    basic_left = 5
+    influence_left = 5
+    influence_explainer = InfluenceExplainer(user_id, user_rated_items, user_algo)
+    for x in recommendations:
+        if x[0] not in used_recommendations:
+            movie_obj = {}
+            movie_obj['movieId'] = int(x[0])
+            movie_obj['title'] = movies_map[int(x[0])]
+            if basic_left > 0:
+                if influence_left > 0:
+                    # rand...
+                    if random.random() > 0.5:
+                        # basic
+                        movie_obj['explanation'] = {}
+                        movie_obj['explanation']['type'] = 'A'
+                        basic_left -= 1
+                    else:
+                        # Influence
+                        positives, negatives = influence_explainer.generate_explanation(x)
+                        movie_obj['explanation'] = {}
+                        movie_obj['explanation']['type'] = 'C'
+                        movie_obj['explanation']['positives'] = [
+                            {'movieId': int(movie_id), 'title': movies_map[int(movie_id)], 'influence': influence} for movie_id, influence in positives]
+                        movie_obj['explanation']['negatives'] = [
+                            {'movieId': int(movie_id), 'title': movies_map[int(movie_id)], 'influence': influence} for movie_id, influence in negatives]
+                        influence_left -= 1
+                else:
+                    # basic
+                    movie_obj['explanation'] = {}
+                    movie_obj['explanation']['type'] = 'A'
+                    basic_left -= 1
+            else :
+                # Influence
+                positives, negatives = influence_explainer.generate_explanation(x)
+                movie_obj['explanation'] = {}
+                movie_obj['explanation']['type'] = 'C'
+                movie_obj['explanation']['positives'] = [
+                    {'movieId': int(movie_id), 'title': movies_map[int(movie_id)], 'influence': influence} for movie_id, influence in positives]
+                movie_obj['explanation']['negatives'] = [
+                    {'movieId': int(movie_id), 'title': movies_map[int(movie_id)], 'influence': influence} for movie_id, influence in negatives]
+                influence_left -= 1
 
-        recommendations_to_send.append(movie_obj)
+            recommendations_to_send.append(movie_obj)
+            if (basic_left == 0 and influence_left == 0):
+                break
     print(str(recommendations_to_send))
     return {'recommendations': recommendations_to_send}
 
@@ -78,13 +136,25 @@ def post_recommendation_responses(user_id):
 
 def initialise():
     global algo
+    global api_key
     with open("algo-20m.pickle", "rb") as fp:
-            algo = pickle.load(fp)
+        algo = pickle.load(fp)
+    with open("../data/apiKey.txt", "r") as fp:
+        api_key = fp.read().replace('\n', '')
+
+    links_df = pd.read_csv("../data/ml-20m/links.csv", dtype={
+                               'movieId': int,
+                               'imdbId': str,
+                               'tmdbId': str,
+                             })
+    for index, row in links_df.iterrows():
+        if 'tmdbId' in row:
+            moviedb_ids[row['movieId']] = row['tmdbId']
     init_movies()
     init_association_rules()
     
 def init_movies():
-    movies_df = pd.read_csv("../data/ml_100k/movies.csv", dtype={
+    movies_df = pd.read_csv("../data/ml-20m/movies.csv", dtype={
                                'movieId': int,
                                'title': str,
                                'genres': str,
@@ -102,8 +172,7 @@ def init_association_rules():
     association_rules = association_rules[(association_rules['support'] > 0.005) & (association_rules['confidence'] > 0.3) 
         & (association_rules['antecedents_length'] < 4) & (association_rules['consequents_length'] == 1)]
 
-def top_n_recommendations(user_id):
-    n = 10
+def top_recommendations(user_id):
     top_n = []
     for i in movies_map:
         # Filter out rated movies
@@ -113,6 +182,13 @@ def top_n_recommendations(user_id):
 
     # Then sort the predictions for each user and retrieve the k highest ones.
     top_n.sort(key=lambda x: x[1], reverse=True)
-    return top_n[:n]
+    return top_n[:1000]
+
+def get_uk_age_rating(release_dates):
+    dates_arr = release_dates['results']
+    for country_info in dates_arr:
+        if country_info['iso_3166_1'] == 'GB':
+            return country_info['release_dates'][-1]['certification']
+    return 'N/A'
 
 initialise()
--- src/explanation.py
+++ src/explanation.py
@@ -4,12 +4,13 @@ class AssociationRulesExplainer():
         self.association_rules = association_rules
 
     def generate_explanation(self, recommendation):
-        rows = self.association_rules.loc[association_rules['consequents']
+        rows = self.association_rules.loc[self.association_rules['consequents']
             .apply(lambda cons: True if recommendation[0] in cons else False)]
         for index, row in rows.iterrows():
             antecedents = list(row['antecedents'])
             if all([x in self.user_rated_items.keys() for x in antecedents]):
-                return antecedents  # This is the explanation 
+                return antecedents  # This is the explanation
+        return None
 
 class InfluenceExplainer():
     def __init__(self, user_id, user_rated_items, user_algo):
